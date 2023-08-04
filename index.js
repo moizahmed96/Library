541a3ce8a43283b953200f03c6d41eccffcbe663
Moiz Ahmed <moizah96@gmail.com>
2023-08-04 18:58:37 -0400

    Display books from Library with Javascript

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-let myLibrary = [];
+const title = document.querySelector(".title")
+const author = document.querySelector(".author")
+const pages = document.querySelector(".pages")
+const read = document.querySelector(".read")
+const submit = document.querySelector("submit")
+
+const formField = document.querySelectorAll(".form-field")
+
+console.log()
+
+const main = document.querySelector(".main")
+
+let myLibrary = [{ title: "avengers", author: "stan lee", pages: "21", read: "true" }];
 
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-
-    this.info = function () {
-        return `${this.title}, ${this.author}, ${this.pages}, ${this.read},`
-    }
 }
 
 function addBookToLibrary() {
-    console.log("hello")
+
+
+    const checkFormField = () => {
+        formField.forEach(field => {
+            return field.textContent
+        })
+    }
+
+    checkFormField()
+
+    myLibrary.forEach(book => {
+        console.log(book)
+        main.append(book.title, book.author, book.pages, book.read)
+    })
 }
+
+addBookToLibrary()
